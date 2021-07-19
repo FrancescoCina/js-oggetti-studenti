97e1f88a785251ec22222a64617baec4544d9b7d
FrancescoCina <francesco.cina04@gmail.com>
2021-07-19 15:36:11 +0200

    Agganciato script e scritta traccia

---
+++ js/es1.js
@@ -0,0 +1,5 @@
+/*
+Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo (in pagina) attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+*/
+
